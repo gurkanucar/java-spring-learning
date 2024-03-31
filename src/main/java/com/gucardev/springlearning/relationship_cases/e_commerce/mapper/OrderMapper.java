2b7754156d3eff13cf29dbc9fc2b5565b31638d1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-03-31 12:20:36 +0300

    order mapper updated

--- src/main/java/com/gucardev/springlearning/relationship_cases/e_commerce/mapper/OrderMapper.java
+++ src/main/java/com/gucardev/springlearning/relationship_cases/e_commerce/mapper/OrderMapper.java
@@ -2,14 +2,17 @@ package com.gucardev.springlearning.relationship_cases.e_commerce.mapper;
 
 import com.gucardev.springlearning.relationship_cases.e_commerce.dto.OrderDTO;
 import com.gucardev.springlearning.relationship_cases.e_commerce.dto.OrderItemDTO;
+import com.gucardev.springlearning.relationship_cases.e_commerce.dto.ProductDTO;
 import com.gucardev.springlearning.relationship_cases.e_commerce.entitiy.Customer;
 import com.gucardev.springlearning.relationship_cases.e_commerce.entitiy.Order;
 import com.gucardev.springlearning.relationship_cases.e_commerce.entitiy.OrderItem;
+import com.gucardev.springlearning.relationship_cases.e_commerce.entitiy.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring"
-        , uses = {ProductMapper.class} // if we comment it we may use toProductDto below -- if we have
-        //     some custom mappings otherwise even if we dont use it will map same fields automatically
+        //    , uses = {ProductMapper.class} // if we comment it we may use toProductDto below -- if we have
+        //       some custom mappings otherwise even if we dont use it will map same fields automatically
 )
 public interface OrderMapper {
     OrderDTO toDto(Order order);
@@ -19,6 +22,7 @@ public interface OrderMapper {
     OrderItemDTO toOrderItemDto(OrderItem orderItem);
 
     // if you dont use `uses` we can convert via this
-    // ProductDTO toProductDto(Product product);
+    @Mapping(target = "optionValues", ignore = true)
+    ProductDTO toProductDto(Product product);
 }
 
