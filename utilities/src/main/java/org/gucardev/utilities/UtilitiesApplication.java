d5d79cfeb9c8313a2fc7d350ab35c7ea7757d917
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-07-15 17:38:12 +0300

    async configuration and OpenApiConfig added

--- utilities/src/main/java/org/gucardev/utilities/UtilitiesApplication.java
+++ utilities/src/main/java/org/gucardev/utilities/UtilitiesApplication.java
@@ -2,7 +2,11 @@ package org.gucardev.utilities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableFeignClients
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class UtilitiesApplication {
 
