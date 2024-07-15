dd55e1f6d46bff38f8e82b1390c961b6920a66b9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-07-15 14:40:25 +0300

    spring scopes examples added

---
+++ spring-scopes/src/main/java/org/gucardev/springscopes/RequestScope.java
@@ -0,0 +1,13 @@
+package com.gucardev.springlearning.scopes;
+
+import lombok.Data;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Scope("request")
+@Component
+@Data
+public class RequestScope {
+    private String value;
+}
