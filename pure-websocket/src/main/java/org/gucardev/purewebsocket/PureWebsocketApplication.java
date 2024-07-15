b8483b4fd08d44efdb387a9b100a98cb521d4bcc
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-07-15 14:28:52 +0300

    websocket project created

---
+++ pure-websocket/src/main/java/org/gucardev/purewebsocket/PureWebsocketApplication.java
@@ -0,0 +1,13 @@
+package org.gucardev.purewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureWebsocketApplication.class, args);
+    }
+
+}
