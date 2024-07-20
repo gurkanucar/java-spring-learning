4d07ecb9e3ac73a5c4ecb4084d77f410ec5afdda
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-07-20 12:46:01 +0300

    bugfix

--- entity-relationship-examples/src/main/java/org/gucardev/entityrelationshipexamples/repository/UserRepository.java
+++ entity-relationship-examples/src/main/java/org/gucardev/entityrelationshipexamples/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.gucardev.entityrelationshipexamples.repository;
 
+import org.gucardev.entityrelationshipexamples.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
