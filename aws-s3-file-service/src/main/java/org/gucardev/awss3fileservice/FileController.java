a2cae19a8b4e2fe4d10cb59295d91d0cacdf8f91
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-08-10 10:56:14 +0300

    some enhancements

--- aws-s3-file-service/src/main/java/org/gucardev/awss3fileservice/FileController.java
+++ aws-s3-file-service/src/main/java/org/gucardev/awss3fileservice/FileController.java
@@ -9,9 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -57,25 +54,13 @@ public class FileController {
     }
 
     @GetMapping("/download/{fileName}")
-    public ResponseEntity<StreamingResponseBody> downloadFile(@PathVariable String fileName) throws IOException {
-        String contentType = Files.probeContentType(Paths.get(fileName));
+    public ResponseEntity<StreamingResponseBody> downloadFile(@PathVariable("fileName") String fileName) throws Exception {
+        return fileService.downloadFileResponse(fileName, false);
+    }
 
-        StreamingResponseBody responseBody = outputStream -> {
-            try (InputStream inputStream = fileService.downloadFile(fileName)) {
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Error while streaming the file", e);
-            }
-        };
-
-        return ResponseEntity.ok()
-                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
-                .header("Content-Type", contentType)
-                .body(responseBody);
+    @GetMapping("/downloadWithWatermark/{fileName}")
+    public ResponseEntity<StreamingResponseBody> downloadFileWithWatermark(@PathVariable("fileName") String fileName) throws Exception {
+        return fileService.downloadFileResponse(fileName, true);
     }
 
 
--- aws-s3-file-service/src/main/java/org/gucardev/awss3fileservice/FileService.java
+++ aws-s3-file-service/src/main/java/org/gucardev/awss3fileservice/FileService.java
@@ -7,16 +7,23 @@ import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.itextpdf.text.DocumentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.Normalizer;
 import java.util.*;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +33,15 @@ public class FileService {
     private String bucketName;
 
     private final AmazonS3 amazonS3;
+    private final WatermarkService watermarkService;
+
+    private String sanitizeFileName(String fileName) {
+        String normalizedFileName = Normalizer.normalize(fileName, Normalizer.Form.NFKD);
+        String fileNameWithUnderscores = normalizedFileName.replaceAll("\\s+", "_");
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9.\\-_]");
+        return pattern.matcher(fileNameWithUnderscores).replaceAll("");
+    }
+
 
     public String generatePreSignedUrl(String filePath, HttpMethod method) {
         GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePresignedUrlRequest(filePath, method);
@@ -52,7 +68,7 @@ public class FileService {
     private GeneratePresignedUrlRequest getGeneratePresignedUrlRequest(String filePath, HttpMethod method) {
         Date expiration = new Date();
         long expTimeMillis = expiration.getTime();
-        expTimeMillis += 1000 * 10; // 60 minutes
+        expTimeMillis += 1000 * 60 * 60; // 60 minutes
         expiration.setTime(expTimeMillis);
         return new GeneratePresignedUrlRequest(bucketName, filePath)
                 .withMethod(method)
@@ -61,7 +77,7 @@ public class FileService {
 
     public String uploadMultipartFile(MultipartFile file) throws IOException {
         File fileObj = convertMultiPartFileToFile(file);
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        String fileName = System.currentTimeMillis() + "_" + sanitizeFileName(file.getOriginalFilename());
         amazonS3.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
         return fileName;
     }
@@ -70,6 +86,32 @@ public class FileService {
         S3Object s3Object = amazonS3.getObject(bucketName, fileName);
         return s3Object.getObjectContent();
     }
+
+    public InputStream downloadFileWithWatermark(String fileName) throws IOException, DocumentException {
+        InputStream originalFileStream = downloadFile(fileName);
+        return watermarkService.addWatermarkToPdf(originalFileStream);
+    }
+
+
+    public ResponseEntity<StreamingResponseBody> downloadFileResponse(String fileName, boolean withWatermark) throws IOException, DocumentException {
+        String contentType = Files.probeContentType(Paths.get(fileName));
+        StreamingResponseBody responseBody = outputStream -> {
+            try (InputStream inputStream = withWatermark ? downloadFileWithWatermark(fileName) : downloadFile(fileName)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            } catch (IOException | DocumentException e) {
+                throw new RuntimeException("Error occurred while streaming the file", e);
+            }
+        };
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .header("Content-Type", contentType)
+                .body(responseBody);
+    }
+
 
     private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
         File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
