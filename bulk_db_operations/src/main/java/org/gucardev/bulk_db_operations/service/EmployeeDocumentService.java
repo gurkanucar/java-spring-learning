b23f13b65a4b61421b4204e01d438c220a563abe
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-08-10 19:59:19 +0300

    async added to bulk db operations example

--- bulk_db_operations/src/main/java/org/gucardev/bulk_db_operations/service/EmployeeDocumentService.java
+++ bulk_db_operations/src/main/java/org/gucardev/bulk_db_operations/service/EmployeeDocumentService.java
@@ -7,11 +7,13 @@ import org.gucardev.bulk_db_operations.model.EmployeeDocument;
 import org.gucardev.bulk_db_operations.repository.EmployeeDocumentRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,20 +33,23 @@ public class EmployeeDocumentService {
         employeeDocumentRepository.save(employeeDocument);
     }
 
+    @Async
     @Transactional
     public void assignDocumentToEmployees(Document document, List<Employee> employees) {
-        List<EmployeeDocument> employeeDocuments = employees.stream()
-                .map(employee -> {
-                    EmployeeDocument employeeDocument = new EmployeeDocument();
-                    employeeDocument.setEmployee(employee);
-                    employeeDocument.setDocument(document);
-                    employeeDocument.setAssignedDate(LocalDateTime.now());
-                    return employeeDocument;
-                })
-                .collect(Collectors.toList());
+        CompletableFuture.runAsync(() -> {
+            List<EmployeeDocument> employeeDocuments = employees.stream()
+                    .map(employee -> {
+                        EmployeeDocument employeeDocument = new EmployeeDocument();
+                        employeeDocument.setEmployee(employee);
+                        employeeDocument.setDocument(document);
+                        employeeDocument.setAssignedDate(LocalDateTime.now());
+                        return employeeDocument;
+                    })
+                    .collect(Collectors.toList());
 
-        employeeDocumentRepository.saveAll(employeeDocuments);
-        System.out.println("Document '" + document.getTitle() + "' has been assigned to " + employees.size() + " employees.");
+            employeeDocumentRepository.saveAll(employeeDocuments);
+            System.out.println("Document '" + document.getTitle() + "' has been assigned to " + employees.size() + " employees.");
+        });
     }
 
 
@@ -60,14 +65,19 @@ public class EmployeeDocumentService {
         System.out.println("Read status for document '" + document.getTitle() + "' has been updated to '" + readStatus + "' for employee '" + employee.getName() + "'.");
     }
 
+    @Async
     @Transactional
     public void assignDocumentToAllEmployees(Document document, Pageable pageable) {
-        Page<Employee> employeePage;
-        do {
-            employeePage = employeeService.getEmployees(pageable);
-            List<Employee> employees = employeePage.getContent();
-            assignDocumentToEmployees(document, employees);
-            pageable = pageable.next();
-        } while (employeePage.hasNext());
+        CompletableFuture.runAsync(() -> {
+            Page<Employee> employeePage;
+            Pageable currentPageable = pageable;
+            do {
+                employeePage = employeeService.getEmployees(currentPageable);
+                List<Employee> employees = employeePage.getContent();
+                assignDocumentToEmployees(document, employees);
+                currentPageable = currentPageable.next();
+            } while (employeePage.hasNext());
+        });
     }
+
 }
