6770b3e4db609b250a70f073bd6253863526e9a3
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-08-20 22:04:18 +0300

    some logging improvements

--- actuator/src/main/java/org/gucardev/actuator/ExceptionAspect.java
+++ actuator/src/main/java/org/gucardev/actuator/ExceptionAspect.java
@@ -7,18 +7,36 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
 @Slf4j
 public class ExceptionAspect {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @AfterThrowing(pointcut = "execution(* org.gucardev.actuator..*(..))" +
             " && !within(org.gucardev.actuator.LogOncePerReqFilter)", throwing = "ex")
     public void handleExceptions(JoinPoint joinPoint, Exception ex) {
         String methodName = joinPoint.getSignature().toShortString();
-        String arguments = Arrays.toString(joinPoint.getArgs());
+        String arguments = formatArguments(joinPoint.getArgs());
         log.error("Method: {}, Arguments: {}, Exception: {}", methodName, arguments, ex.getMessage(), ex);
     }
 
+    private String formatArguments(Object[] args) {
+        try {
+            return objectMapper.writeValueAsString(args);
+        } catch (JsonProcessingException e) {
+            return Arrays.toString(args);
+        }
+    }
 }
